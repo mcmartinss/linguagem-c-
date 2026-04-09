fdb024d07168414ab22902bc6b49c5cca7538c29
Maria Clara <maclasilva07@gmail.com>
2026-04-09 17:25:02 -0300

    Add files via upload

---
+++ exercicioOrdem.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a, b;
+	
+	printf("Digite dois numeros: \n");
+	scanf("%d", &a);
+	scanf("%d", &b);
+	
+	while (a != b){
+		if (a<b){
+			printf("CRESCENTE\n");
+		} else {
+			printf("DECRESCENTE\n");
+		}
+		
+	printf("Digite outros dois numeros: \n");
+	scanf("%d", &a);
+	scanf("%d", &b);
+	}
+	
+return 0;
+
+}
